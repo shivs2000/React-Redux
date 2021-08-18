750ebe6a0f9f82bd2c8f20f50e54ec1130e29c14
shivs2000 <singhshivam0666@gmail.com>
2021-08-18 23:30:43 +0530

    second commit

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const CakeContainer = () => {
+    return ( 
+      <div>
+       <h2>Number of cakes</h2>
+       <button>buy cake</button>
+      </div> 
+      );
+}
+ 
+export default CakeContainer;
---
+++ src/redux/cake/CakeActions.js
@@ -0,0 +1,11 @@
+import { BUY_CAKE } from "./CakeTypes";
+
+const CakeActions = () => {
+    return ( 
+        {
+            type:BUY_CAKE,
+        }
+     );
+}
+ 
+export default CakeActions;
